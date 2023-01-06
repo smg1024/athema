db0b91e6911a54c98cd1fdc909d07fa698213b73
Poby <smg981024@gmail.com>
2023-01-06 16:52:15 +0900

    Athema Initialize

---
+++ athema/src/main/java/com/athema/AthemaApplication.java
@@ -0,0 +1,13 @@
+package com.athema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AthemaApplication.class, args);
+	}
+
+}
---
+++ athema/src/test/java/com/athema/AthemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.athema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AthemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
