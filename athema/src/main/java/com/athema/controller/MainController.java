0d94c314190756a529e504a2dc5433dda0a9af79
Poby <smg981024@gmail.com>
2023-01-09 14:32:37 +0900

    상민 230109 index.html, MainController.java
    index.html에 있는 링크 thymeleaf로 변경 후 MainController.java에서 매핑 완료
    **tour.html은 없음

--- athema/src/main/java/com/athema/controller/MainController.java
+++ athema/src/main/java/com/athema/controller/MainController.java
@@ -5,8 +5,58 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
-	@RequestMapping("")
+	@RequestMapping("/index")
 	public String index() {
 		return "index";
+	}
+	
+	@RequestMapping("/about")
+	public String about() {
+		return "about";
+	}
+	
+	@RequestMapping("/tours")
+	public String tours() {
+		return "tours";
+	}
+	
+	@RequestMapping("/blog-default")
+	public String blogdefault() {
+		return "blog-default";
+	}
+	
+	@RequestMapping("/blog-single")
+	public String blogsingle() {
+		return "blog-single";
+	}
+	
+	@RequestMapping("/testimonials")
+	public String testimonials() {
+		return "testimonials";
+	}
+	
+	@RequestMapping("/contact-us")
+	public String contactus() {
+		return "contact-us";
+	}
+	
+	@RequestMapping("/styleguide")
+	public String styleguide() {
+		return "styleguide";
+	}
+	
+	@RequestMapping("/domestic-tour")
+	public String domestictour() {
+		return "domestic-tour";
+	}
+
+	@RequestMapping("/international-tour")
+	public String internationaltour() {
+		return "international-tour";
+	}
+	
+	@RequestMapping("/asdf")
+	public String asdf() {
+		return "asdf";
 	}
 }
