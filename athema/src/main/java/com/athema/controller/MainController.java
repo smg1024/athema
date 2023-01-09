f8ea6e54d3c2fa21d3b38db557661587902709a0
Poby <smg981024@gmail.com>
2023-01-09 16:53:27 +0900

    상민 230109
    tours를 제외한 모든 페이지 th:insert로 구성 및 매핑 완

--- athema/src/main/java/com/athema/controller/MainController.java
+++ athema/src/main/java/com/athema/controller/MainController.java
@@ -13,28 +13,33 @@ public class MainController {
 	}
 	
 	@RequestMapping("/about")
-	public String about() {
-		return "about";
+	public String about(Model model) {
+		model.addAttribute("center", "about");
+		return "index";
 	}
 	
 	@RequestMapping("/tours")
-	public String tours() {
-		return "tours";
+	public String tours(Model model) {
+		model.addAttribute("center", "tours");
+		return "index";
 	}
 	
 	@RequestMapping("/blog-default")
-	public String blogdefault() {
-		return "blog-default";
+	public String blogdefault(Model model) {
+		model.addAttribute("center", "blog-default");
+		return "index";
 	}
 	
 	@RequestMapping("/blog-single")
-	public String blogsingle() {
-		return "blog-single";
+	public String blogsingle(Model model) {
+		model.addAttribute("center", "blog-single");
+		return "index";
 	}
 	
 	@RequestMapping("/testimonials")
-	public String testimonials() {
-		return "testimonials";
+	public String testimonials(Model model) {
+		model.addAttribute("center", "testimonials");
+		return "index";
 	}
 	
 	@RequestMapping("/contact-us")
@@ -44,22 +49,25 @@ public class MainController {
 	}
 	
 	@RequestMapping("/styleguide")
-	public String styleguide() {
-		return "styleguide";
+	public String styleguide(Model model) {
+		model.addAttribute("center", "styleguide");
+		return "index";
 	}
 	
 	@RequestMapping("/domestic-tour")
-	public String domestictour() {
-		return "domestic-tour";
+	public String domestictour(Model model) {
+		model.addAttribute("center", "domestic-tour");
+		return "index";
 	}
 
 	@RequestMapping("/international-tour")
-	public String internationaltour() {
-		return "international-tour";
+	public String internationaltour(Model model) {
+		model.addAttribute("center", "international-tour");
+		return "index";
 	}
 	
 	@RequestMapping("/asdf")
-	public String asdf() {
+	public String asdf(Model model) {
 		return "asdf";
 	}
 }
