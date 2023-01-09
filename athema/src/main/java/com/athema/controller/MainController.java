481ff2cdf4223f5cbb4ebb182861d381d1a4e3c5
Poby <smg981024@gmail.com>
2023-01-09 09:10:19 +0900

    상민 230109 부트스트랩 테마 초안

---
+++ athema/src/main/java/com/athema/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.athema.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	@RequestMapping("")
+	public String index() {
+		return "index";
+	}
+}
