0a71f606fd40f0a5e811b241b245fbe5b1ff3d8d
Poby <smg981024@gmail.com>
2023-01-11 16:39:44 +0900

    230111 상민
    Thymeleaf는 서버와 데이터를 주고 받는 부분에만 사용하는 방식으로 해야할 것 같습니다.
    한번 리셋하고 index.html에 있는 href 주소만 다듬은 후 MainController.java에 매핑한 상태입니다
    각자 pull하시고 맡으신 기능에 대한 페이지 구성 시작하시면 될 것 같습니다.

--- athema/src/main/java/com/athema/controller/MainController.java
+++ athema/src/main/java/com/athema/controller/MainController.java
@@ -7,63 +7,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 	@RequestMapping("/index")
-	public String index(Model model) {
-		model.addAttribute("center", "maincenter");
+	public String index() {
 		return "index";
 	}
 	
 	@RequestMapping("/about")
 	public String about() {
-		
 		return "about";
 	}
 	
-	@RequestMapping("/tours")
-	public String tours(Model model) {
-		model.addAttribute("center", "tours");
-		return "index";
+	@RequestMapping("/offers")
+	public String tours() {
+		return "offers";
 	}
 	
-	@RequestMapping("/blog-default")
-	public String blogdefault(Model model) {
-		model.addAttribute("center", "blog-default");
-		return "index";
+	@RequestMapping("/blog")
+	public String blogdefault() {
+		return "blog";
 	}
 	
-	@RequestMapping("/blog-single")
-	public String blogsingle(Model model) {
-		model.addAttribute("center", "blog-single");
-		return "index";
-	}
-	
-	@RequestMapping("/testimonials")
-	public String testimonials(Model model) {
-		model.addAttribute("center", "testimonials");
-		return "index";
-	}
-	
-	@RequestMapping("/contact-us")
-	public String contactus(Model model) {
-		model.addAttribute("center", "contact-us");
-		return "index";
-	}
-	
-	@RequestMapping("/styleguide")
-	public String styleguide(Model model) {
-		model.addAttribute("center", "styleguide");
-		return "index";
-	}
-	
-	@RequestMapping("/domestic-tour")
-	public String domestictour(Model model) {
-		model.addAttribute("center", "domestic-tour");
-		return "index";
-	}
-
-	@RequestMapping("/international-tour")
-	public String internationaltour(Model model) {
-		model.addAttribute("center", "international-tour");
-		return "index";
+	@RequestMapping("/contact")
+	public String blogsingle() {
+		return "contact";
 	}
 	
 	@RequestMapping("/asdf")
