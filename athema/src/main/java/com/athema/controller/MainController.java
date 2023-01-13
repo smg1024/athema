2a5723760b5dade929f3e8cc491e0687675c6b3e
hyo <itshyoj@gmail.com>
2023-01-13 15:48:25 +0900

    23.01.13. 템플릿 Model 형식으로 전체 수정 및 로그인 페이지 화면구성
    Index 페이지와 Main 페이지 분리
    컨트롤러에서 Model 객체 형식으로 변경
    로그인 페이지 화면제작

--- athema/src/main/java/com/athema/controller/MainController.java
+++ athema/src/main/java/com/athema/controller/MainController.java
@@ -6,34 +6,68 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
+	
+	@RequestMapping("")
+	public String main() {
+		return "index";
+	}
+	
+	@RequestMapping("/main")
+	public String main2() {
+		return "index";
+	}
+
 	@RequestMapping("/index")
 	public String index() {
 		return "index";
 	}
 	
+	@RequestMapping("/login")
+	public String login(Model model) {
+		model.addAttribute("content", "login");
+		return "main";
+	}
+	
+	@RequestMapping("/register")
+	public String register(Model model) {
+		model.addAttribute("content", "register");
+		return "main";
+	}
+	
 	@RequestMapping("/about")
-	public String about() {
-		return "about";
+	public String about(Model model) {
+		model.addAttribute("content", "about");
+		return "main";
 	}
 	
 	@RequestMapping("/offers")
-	public String tours() {
-		return "offers";
+	public String offers(Model model) {
+		model.addAttribute("content", "offers");
+		return "main";
 	}
 	
 	@RequestMapping("/blog")
-	public String blogdefault() {
-		return "blog";
+	public String blog(Model model) {
+		model.addAttribute("content", "blog");
+		return "main";
 	}
 	
 	@RequestMapping("/contact")
-	public String blogsingle() {
-		return "contact";
+	public String contact(Model model) {
+		model.addAttribute("content", "contact");
+		return "main";
 	}
 	
-	@RequestMapping("/asdf")
-	public String asdf(Model model) {
-		return "asdf";
+	@RequestMapping("/elements")
+	public String elements(Model model) {
+		model.addAttribute("content", "elements");
+		return "main";
+	}
+
+	@RequestMapping("/single_listing")
+	public String single_listing(Model model) {
+		model.addAttribute("content", "single_listing");
+		return "main";
 	}
 	
 }
