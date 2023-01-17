12745eb66cb9b8750686b2dd21b688606b302f1c
hyo <itshyoj@gmail.com>
2023-01-17 19:44:18 +0900

    230116 효정 | 카카오 로그인 API 연동 구현 중 (RESET)
    버튼 이미지 추가 및 자바스크립트 추가
    카카오계정 로그인 화면 연동 완료
    (ING) Controller에서 액세스 토큰 요청기능

--- athema/src/main/java/com/athema/controller/MemberController.java
+++ athema/src/main/java/com/athema/controller/MemberController.java
@@ -1,12 +1,46 @@
 package com.athema.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-@Controller
+@RestController
 @RequestMapping("/member")
 public class MemberController {
+	
+	@RequestMapping("/kakao")
+	public void callback(@RequestParam String code) {
+		System.out.println(code);
+		
+		// Post 방식으로 Access Token 요청하기 (key=value)
+		RestTemplate rt = new RestTemplate();
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+		
+		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+		params.add("grant_type", "authorization_code");
+		params.add("client_id", "9740cda4f593e49b4bf7b3235435574d");
+		params.add("redirect_uri", "http://127.0.0.1/member/kakao");
+		params.add("code", code);
+		
+		// rt에 HttpHeader와 HttpBody 모두를 한번에 담아주기 위해 HttpEntity 객체 생성
+		HttpEntity<MultiValueMap<String, String>> kakaoTokenReq = new HttpEntity<>(headers, params);
+		
+		// HttpEntity를 담아서 post방식으로 http 요청 및 응답 저장
+		ResponseEntity<String> kakaoTokenRes = rt.exchange("https://kauth.kakao.com/oauth/token", HttpMethod.POST, kakaoTokenReq, String.class);
+		
+		// 이후 어떻게 추출? -> 아직 작성 못함
+		
+	}
 	
 	@RequestMapping("/welcome")
 	public String welcome(Model model) {
