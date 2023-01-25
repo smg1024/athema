d8b7b5590ceb298a3b98277b9a3a5294d34effa8
Poby <smg981024@gmail.com>
2023-01-25 11:39:53 +0900

    230125 상민 관리자 프로젝트 생성
    수업에서 진행한 Boostrap 테마 적용
    Websocket dependency 추가
    포트 8080, context-path=/admin

---
+++ athema/src/main/java/com/athema/controller/OffersController.java
@@ -0,0 +1,8 @@
+package com.athema.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class OffersController {
+	
+}
---
+++ athema_admin/src/main/java/com/athema/AthemaAdminApplication.java
@@ -0,0 +1,13 @@
+package com.athema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthemaAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AthemaAdminApplication.class, args);
+	}
+
+}
