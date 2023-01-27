3e7d22f130dbccf92ad0b6a6200bc61d81e87f59
minha <minha@userA>
2023-01-27 15:02:04 +0900

    230127 민하

--- athema/src/main/java/com/athema/controller/OrderController.java
+++ athema/src/main/java/com/athema/controller/OrderController.java
@@ -1,14 +1,32 @@
 package com.athema.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.athema.dto.ItemDTO;
+import com.athema.service.ItemService;
 import com.athema.service.OrderService;
 
 @Controller
 @RequestMapping("/order")
 public class OrderController {
 
+	@Autowired
+	OrderService oservice;
+	
+	@Autowired
+	ItemService iservice;
+	
+
+	
+	@RequestMapping("/bookregister")
+	public String register() {
+		return null;
+	}
+	 
 
 }
