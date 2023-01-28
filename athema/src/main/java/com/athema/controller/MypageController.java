2e747c74512c08fd0db00b90cf5b8e686ddf8e71
minha <minha@userA>
2023-01-28 14:41:12 +0900

    230128 민하 mypage틀

--- athema/src/main/java/com/athema/controller/MypageController.java
+++ athema/src/main/java/com/athema/controller/MypageController.java
@@ -12,9 +12,9 @@ public class MypageController {
 	
 	@RequestMapping("")
 	public String mypage(Model model) {
-		model.addAttribute("content", "mypage");
-		model.addAttribute("mypageleft", "mypageleft");
-		model.addAttribute("mypagecenter", "mypagecenter");
+		model.addAttribute("content", "mypage");	
+		model.addAttribute("mypageleft", "left");	//왼쪽이 mypageleft.html 가져오겠다 오른쪽이 mypage.html에서 쓰는 이름
+		model.addAttribute("mypagecenter", "center");
 		return "main";
 	}
 	
