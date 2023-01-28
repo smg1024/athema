276503be76bd9c4cd763ba6c4c5395c2e6b32a4b
minha <minha@userA>
2023-01-28 16:46:59 +0900

    230128 민하 마이페이지 매핑 완료

--- athema/src/main/java/com/athema/controller/MypageController.java
+++ athema/src/main/java/com/athema/controller/MypageController.java
@@ -13,8 +13,49 @@ public class MypageController {
 	@RequestMapping("")
 	public String mypage(Model model) {
 		model.addAttribute("content", "mypage");	
-		model.addAttribute("mypageleft", "left");	//왼쪽이 mypageleft.html 가져오겠다 오른쪽이 mypage.html에서 쓰는 이름
-		model.addAttribute("mypagecenter", "center");
+		model.addAttribute("mypagecenter", "mypagecenter");
+		return "main";
+	}
+	//http://127.0.0.1/mypage/userpage
+	@RequestMapping("/userpage")
+	public String userpage(Model model) {	
+		model.addAttribute("content", "mypage");	
+		model.addAttribute("mypagecenter",dir+"userpage");
+		return "main";
+	}
+	
+	@RequestMapping("/travel_list")
+	public String travel_list(Model model) {	
+		model.addAttribute("content", "mypage");	
+		model.addAttribute("mypagecenter",dir+"travel_list");
+		return "main";
+	}
+	
+	@RequestMapping("/travel_plan")
+	public String travel_plan(Model model) {	
+		model.addAttribute("content", "mypage");	
+		model.addAttribute("mypagecenter",dir+"travel_plan");
+		return "main";
+	}
+	
+	@RequestMapping("/travel_past")
+	public String travel_past(Model model) {	
+		model.addAttribute("content", "mypage");	
+		model.addAttribute("mypagecenter",dir+"travel_past");
+		return "main";
+	}
+	
+	@RequestMapping("/myreview")
+	public String myreview(Model model) {	
+		model.addAttribute("content", "mypage");	
+		model.addAttribute("mypagecenter",dir+"myreview");
+		return "main";
+	}
+	
+	@RequestMapping("/mywishlist")
+	public String mywishlist(Model model) {	
+		model.addAttribute("content", "mypage");	
+		model.addAttribute("mypagecenter",dir+"mywishlist");
 		return "main";
 	}
 	
